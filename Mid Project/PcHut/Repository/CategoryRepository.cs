f573fa4d975f5d3efc6eafd36bbca43aef8215b7
Born0 <bornoahmed2@gmail.com>
2021-03-12 21:05:45 +0600

    Mid project begin

---
+++ Mid Project/PcHut/Repository/CategoryRepository.cs
@@ -0,0 +1,13 @@
+﻿using PcHut.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PcHut.Repository
+{
+    public class CategoryRepository : Repository<category>
+    {
+
+    }
+}
---
+++ Mid Project/PcHut/Repository/ProductRepository.cs
@@ -0,0 +1,16 @@
+﻿using PcHut.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PcHut.Repository
+{
+    public class ProductRepository : Repository<product>
+    {
+        public List<product> GetTopProducts(int top)
+        {
+            return this.context.products.OrderByDescending(x => x.price).Take(top).ToList();
+        }
+    }
+}
